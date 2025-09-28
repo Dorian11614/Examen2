aa324b3227d584ac13b2794fe76dfead80449ea4
Dorian Agustin <143836424+Dorian11614@users.noreply.github.com>
2025-09-28 15:16:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,26 +3,28 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+// Importar rutas
 const clientesRoutes = require("./routes/clientes");
 const ordenesRoutes = require("./routes/ordenes");
 
 const app = express();
 
-// Middlewares
+// ===== Middlewares =====
 app.use(cors());
 app.use(express.json());
 
-// Rutas
+// ===== Rutas API =====
 app.use("/clientes", clientesRoutes);
 app.use("/ordenes", ordenesRoutes);
 
-// Ruta raíz para probar que funciona
+// ===== Ruta de prueba =====
 app.get("/", (req, res) => {
-  res.send("🚀 API Restaurante funcionando en Render");
+  res.send(" API Restaurante funcionando en Render");
 });
 
-// Puerto
+// ===== Puerto =====
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`✅ Servidor corriendo en puerto ${PORT}`);
+  console.log(` Servidor corriendo en puerto ${PORT}`);
 });
+
