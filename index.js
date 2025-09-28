9f268b798c972864a2b8de88eb79801baf9a1077
Dorian Agustin <143836424+Dorian11614@users.noreply.github.com>
2025-09-28 15:28:04 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,18 +13,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// ===== Frontend estático =====
+app.use(express.static("public"));
+
 // ===== Rutas API =====
 app.use("/clientes", clientesRoutes);
 app.use("/ordenes", ordenesRoutes);
 
 // ===== Ruta de prueba =====
-app.get("/", (req, res) => {
-  res.send(" API Restaurante funcionando en Render");
+app.get("/api", (req, res) => {
+  res.send("🚀 API Restaurante funcionando en Render");
 });
 
 // ===== Puerto =====
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(` Servidor corriendo en puerto ${PORT}`);
+  console.log(`✅ Servidor corriendo en puerto ${PORT}`);
 });
 
